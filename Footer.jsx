fe4223f3d62e7ef1cdd015d2394c2524fa9f7b10
NIRAV MOJAGAR <69744741+nirav-Hub4yoU@users.noreply.github.com>
2021-06-05 14:03:47 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+
+function Footer(){
+
+  var currYear =  new Date().getFullYear();
+
+  return(
+    <Footer>
+     <p>Copyright @ 2021{currYear}</p>
+
+    
+</Footer>
+  );
+
+
+}
+
+export default Footer;
